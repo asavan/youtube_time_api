24a3ea15308a817b522a0e5ef77b322cc60de5cd
a.savanovich <asavanster@gmail.com>
2026-08-30 22:17:40 +0300

    clean trash with adb

---
+++ find_trash_not_in_today.js
@@ -0,0 +1,125 @@
+import {open} from "node:fs/promises";
+import {promisify} from "node:util";
+import {execFile} from "node:child_process";
+
+import {allReaderBinded} from "./all_reader.js";
+import {full_url} from "./youtube_url.js";
+
+const execFilePromise = promisify(execFile);
+
+async function extractVideoIds3(filename) {
+    const file = await open(filename);
+    const videoIds = [];
+
+    try {
+        for await (const line of file.readLines()) {
+            const trimmedLine = line.trim();
+
+            const closeBracketIdx = trimmedLine.lastIndexOf(']');
+            const openBracketIdx = trimmedLine.lastIndexOf('[', closeBracketIdx);
+
+            if (openBracketIdx !== -1 && closeBracketIdx !== -1 && closeBracketIdx > openBracketIdx) {
+                const videoId = trimmedLine.substring(openBracketIdx + 1, closeBracketIdx);
+                if (videoId) {
+                    videoIds.push(videoId);
+                }
+            }
+        }
+    } finally {
+        await file?.close();
+    }
+    return videoIds;
+}
+
+async function extractVideoIds4(bigString) {
+    const videoIds = [];
+
+    for (const line of bigString.split("\n")) {
+        const trimmedLine = line.trim();
+        if (trimmedLine === "") {
+            continue;
+        }
+
+        const closeBracketIdx = trimmedLine.lastIndexOf(']');
+        const openBracketIdx = trimmedLine.lastIndexOf('[', closeBracketIdx);
+
+        if (openBracketIdx !== -1 && closeBracketIdx !== -1 && closeBracketIdx > openBracketIdx) {
+            const videoId = trimmedLine.substring(openBracketIdx + 1, closeBracketIdx);
+            if (videoId) {
+                videoIds.push(videoId);
+            }
+        }
+    }
+
+    return videoIds;
+}
+
+function getDifference(arr1, arr2) {
+    const set2 = new Set(arr2);
+    return arr1.filter(item => !set2.has(item));
+}
+
+function getDailyNotePath(prefix, date = new Date()) {
+    const dateStr = date.toLocaleDateString('en-CA');
+    return `${prefix}${dateStr}.md`;
+}
+
+
+async function main() {
+    try {
+        const args = [];
+        if (process.env.ANDROID_DEVICE_ID) {
+            args.push("-s");
+            args.push(process.env.ANDROID_DEVICE_ID);
+        }
+        args.push("shell");
+        args.push("ls");
+        args.push(process.env.ANDROID_FOLDER);
+        const {stdout, stderr} = await execFilePromise(process.env.ADB_LOCATION || "adb", args);
+
+        if (stderr) {
+            console.warn(stderr);
+        }
+
+        const ids = await extractVideoIds4(stdout);
+        if (ids.length === 0) {
+            console.log("Корзина пуста");
+            return;
+        }
+
+        const file = process.argv[2] || getDailyNotePath(process.env.OBSIDIAN_DAILY_FOLDER);
+        const mask = Number.parseInt(process.argv[3]) || 3;
+        const reader = allReaderBinded(mask);
+        const idsToday = await reader(file);
+
+        console.log(`Обработка завершена. Найдено ID: ${ids.length}`);
+
+        const idsNotToday = getDifference(ids, idsToday);
+        console.log('Список ID:', idsNotToday);
+        if (idsNotToday.length === 0) {
+            // clear trash
+            const args = [];
+            if (process.env.ANDROID_DEVICE_ID) {
+                args.push("-s");
+                args.push(process.env.ANDROID_DEVICE_ID);
+            }
+            args.push("shell");
+            args.push("rm");
+            args.push(process.env.ANDROID_FOLDER + "/*");
+            const {stdout, stderr} = await execFilePromise(process.env.ADB_LOCATION || "adb", args);
+
+            if (stderr) {
+                console.warn(stderr);
+            }
+            console.log(stdout);
+            return;
+        }
+        const linksNotToday = idsNotToday.map(full_url);
+        console.log(linksNotToday.join('\n'));
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+main();
